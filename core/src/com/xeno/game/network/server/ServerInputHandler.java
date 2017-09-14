1bd8ba40ecdba6c43c082f0325f2010e86535413
Gianni <gianni.infi@gmail.com>
2017-09-14 16:05:18 -0400

    More implementation

--- core/src/com/xeno/game/network/server/ServerInputHandler.java
+++ core/src/com/xeno/game/network/server/ServerInputHandler.java
@@ -12,8 +12,13 @@ public class ServerInputHandler {
 	public void handleInput(String input) {
 		String[] command = input.split(" ");
 		
+		if (command[0].startsWith("/"))
+			command[0] = command[0].substring(1);
+		else
+			System.out.println("Please enter a valid command. \nType '/commands' to view available commands.");
+		
 		switch(command[0]) {
-			case "/warp":
+			case "warp":
 				warp(command[1], Integer.parseInt(command[2]));
 				break;
 			default: 
