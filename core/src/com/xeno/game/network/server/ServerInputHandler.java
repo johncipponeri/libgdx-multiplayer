3a7d9f819f0888a81131c4b270a8b5b3ca05f421
Gianni <gianni.infi@gmail.com>
2017-09-14 19:13:44 -0400

    Crash handling + interactivity

--- core/src/com/xeno/game/network/server/ServerInputHandler.java
+++ core/src/com/xeno/game/network/server/ServerInputHandler.java
@@ -19,14 +19,41 @@ public class ServerInputHandler {
 		
 		switch(command[0]) {
 			case "warp":
-				warp(command[1], Integer.parseInt(command[2]));
+				handleWarp(command);
 				break;
 			default: 
 				System.out.println("Command " + command[0] + " does not exist.");
 		}
 	}
 	
+	public void handleWarp(String[] input) {
+		
+		if (input.length == 1) {
+			System.out.println("/warp playername mapid");
+			return;
+		}
+		if (input.length != 3 && input.length != 1) {
+			System.out.println("Invalid amount of arguments.\nArguments Expected: " + "3\n" + "Arguments given: " + input.length);
+			return;
+		}
+		
+		if(isParsable(input[2]))
+			warp(input[1], Integer.parseInt(input[2]));
+		else
+			System.out.println(input[2] + " is not a Map ID.");
+	}
+	
 	public void warp(String name, int mapID) {
 		System.out.println("Warping " + name + " to map #" + mapID);
+	}
+	
+	public boolean isParsable(String input){
+	    boolean parsable = true;
+	    try{
+	        Integer.parseInt(input);
+	    }catch(NumberFormatException e){
+	        parsable = false;
+	    }
+	    return parsable;
 	}
 }
